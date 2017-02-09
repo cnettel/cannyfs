e92987923d3f15bba58a229e3a0a0514381f25c4
cnettel <carl.nettelblad@it.uu.se>
2017-02-09 01:49:16 +0100

    Extreme vetting and explicit casting

--- cannyfs.cpp
+++ cannyfs.cpp
@@ -663,8 +663,8 @@ static int cannyfs_getattr(const char *path, struct stat *stbuf)
 
 	if (inaccurate)
 	{
-		if (options.verbose && b.fileobj) fprintf(stderr, "Inner file obj %llx for %s, missing status %d, would report %d\n", b.fileobj, path, (int)b.fileobj->missing, (int)(options.cachemissing && b.fileobj && b.fileobj->missing));
-		if (options.cachemissing && b.fileobj && b.fileobj->missing)
+		if (options.verbose && b.fileobj) fprintf(stderr, "Inner file obj %llx for %s, missing status %d, would report %d\n", b.fileobj, path, (int)b.fileobj->missing, options.cachemissing);
+		if (options.cachemissing && b.fileobj && (bool) b.fileobj->missing)
 		{
 			if (options.verbose) fprintf(stderr, "Reporting %s to be missing\n", path);
 			return -ENOENT;
