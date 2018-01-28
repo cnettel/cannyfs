cb445cd6f9f62754df1dde47ed8f08e6b03e699b
cnettel <carl.nettelblad@it.uu.se>
2018-01-28 14:20:39 +0100

    Enable kernel cache

--- cannywrapper.sh
+++ cannywrapper.sh
@@ -14,7 +14,7 @@ ulimit -u 10000
 
 CANNY_PATH=`mktemp -d -t cannywrapper.XXXXXXXXXX || exit 1`
 echo Temporary mount in $CANNY_PATH
-./cannyfs -f -o big_writes -o max_write=65536 $CANNY_PATH &
+./cannyfs -f -o kernel_cache -o big_writes -o max_write=65536 $CANNY_PATH &
 until mountpoint -q $CANNY_PATH; do sleep 1 && echo "Waiting for mountpoint"; done
 
 # Since we trust ourselves and use chroot for convenience, fakechroot is fine
